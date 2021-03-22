451325c36e9c72463cd85ae23b1d7afe5dec79ce
許皓維 <c9dk.012@gmail.com>
2021-03-22 12:20:38 +0800

    Update README.md

--- Geospatial Analysis/Tutorial.py
+++ Geospatial Analysis/Tutorial.py
@@ -3,5 +3,29 @@ import geopandas as gpd
 # Read in the data
 full_data = gpd.read_file('./DEClands.shp')
 
-# View the first five rows of the data
-full_data.head()
+data = full_data.loc[:, ["CLASS", "COUNTY", "geometry"]].copy()
+
+# Select lands that fall under the "WILD FOREST" or "WILDERNESS" category
+wild_lands = data.loc[data.CLASS.isin(['WILD FOREST', 'WILDERNESS'])].copy()
+
+# Create map
+wild_lands.plot()
+
+# Campsites in New York state (Point)
+POI_data = gpd.read_file("./Decptsofinterest.shp")
+campsites = POI_data.loc[POI_data.ASSET=='PRIMITIVE CAMPSITE'].copy()
+
+# Foot trails in New York state (LineString)
+roads_trails = gpd.read_file("./Decroadstrails.shp")
+trails = roads_trails.loc[roads_trails.ASSET=='FOOT TRAIL'].copy()
+
+# County boundaries in New York state (Polygon)
+counties = gpd.read_file("./NY_county_boundaries.shp")
+
+# Define a base map with county boundaries
+ax = counties.plot(figsize=(10,10), color='none', edgecolor='gainsboro', zorder=3)
+
+# Add wild lands, campsites, and foot trails to the base map
+wild_lands.plot(color='lightgreen', ax=ax)
+campsites.plot(color='maroon', markersize=2, ax=ax)
+trails.plot(color='black', markersize=1, ax=ax)
